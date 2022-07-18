5bc665c4e1f095387dd58e703349d334ca8f2da7
ultragrace <udechukwuakachukwu.com>
2022-07-18 11:03:16 +0100

    Basic Folder structure created and arranged

--- database/connection.js
+++ database/connection.js
@@ -2,11 +2,11 @@ import { createPool } from 'mysql'
 
 // creating
 const pool = createPool({
-  port: 3306,
-  host: localhost,
-  user: 'root',
-  password: '',
-  database: 'medical-app-dp',
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   connectionLimit: 10
 })
 export default pool
